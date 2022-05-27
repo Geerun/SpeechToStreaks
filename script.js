35a209c6d8d258d8f36d7fcfc3a24ea52802b980
Geerun <bestgeerun@gmail.com>
2022-05-27 22:04:49 +0400

    fix: edit makeSlot function

--- script.js
+++ script.js
@@ -12,17 +12,14 @@ const r = [0, 0, 0, 0, 0, 0, 0, 0, 0, 'S200087909'];
 
 function makeSlot() {for (let i = 10; i <=142; i++){
     t[i]=('S' + (200088170+i)); 
+    console.log(' t '+i + ' '+t[i])
     s[i]=('S' + (200088035+i));
+    console.log(' s '+i + ' '+s[i])
     r[i]=('S' + (200087900+i));
+    console.log(' r '+i + ' '+r[i])
 }
 }
 makeSlot();
-//     a37: 'S200088342',    р29: 'S200087929',    р30: 'S200087930',    r50: 'S200087950',    r55: 'S200087955',    r36: 'S200087936',    
-//     r73: 'S200087973',    r10: 'S200087910',    r101: 'S200088001',    r142: 'S200088042',    t141: 'S200088311',    t142: 'S200088312',
-//     т10: 'S200088180',    t13: 'S200088183',    т15: 'S200088185',    т25: 'S200088195',    т50: 'S200088220', t121: 'S200088291',    
-//     т98: 'S200088268',    c142: 'S200088177',    s10: 'S200088045',    с30: 'S200088065',    s31: 'S200088066',    s120: 'S200088155',    
-//     s97: 'S200088132',
-
 recognition.continuous = true;
 recognition.start();
 
@@ -34,17 +31,17 @@ recognition.onresult = function(event) {
     var transcript = event.results[current][0].transcript;
        sl = transcript.toLowerCase().replace(/[^0-9]/gim,'');
        w = transcript.toLowerCase().replace(/[^а-я,a-z,А-Я,A-Z]/gim,'');
-        console.log(sl)
-        console.log(w)
+        
+        console.log(w + sl)
 
         switch (w) {
-            case 't','т':
+            case 't' || 'т':
                 getBarcode(t)
                 break;
-            case 'с','s':
+            case 'с' || 's':
                 getBarcode(s)
                 break;
-            case 'r','р':
+            case 'r' || 'р':
                 getBarcode(r)
                     break;
         
