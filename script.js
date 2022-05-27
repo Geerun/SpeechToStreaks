4719762f177b107e6723d206638df54d2692ba0c
Geerun <bestgeerun@gmail.com>
2022-05-27 23:03:54 +0400

    release version

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-
 var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 var recognition = new SpeechRecognition();
 var noteTextarea = $('#result-area');
-var noteContent = '';
 let slot = '',
 sl = '',
 w = '';
@@ -20,41 +18,6 @@ function makeSlot() {for (let i = 10; i <=142; i++){
 }
 }
 makeSlot();
-recognition.continuous = true;
-recognition.start();
-
-// This block is called every time the Speech APi captures a line. Ф
-recognition.onresult = function(event) {
-    var noteContent = '';
-    var current = event.resultIndex;
-    // Get a transcript of what was said.
-    var transcript = event.results[current][0].transcript;
-       sl = transcript.toLowerCase().replace(/[^0-9]/gim,'');
-       w = transcript.toLowerCase().replace(/[^а-я,a-z,А-Я,A-Z]/gim,'');
-        
-        console.log(w + sl)
-
-        switch (w) {
-            case 't' || 'т':
-                getBarcode(t)
-                break;
-            case 'с' || 's':
-                getBarcode(s)
-                break;
-            case 'r' || 'р':
-                getBarcode(r)
-                    break;
-        
-            default:
-                break;
-        }
-        
-   
-    var mobileRepeatBug = (current == 1 && transcript == event.results[0][0].transcript);
-    if (!mobileRepeatBug) {
-        noteTextarea.val(sl);
-    }
-};
 
 function getBarcode(row){
     slot = row[sl]
@@ -62,11 +25,46 @@ function getBarcode(row){
     document.getElementById('shtrihPicture').src = source;
 }
 
-$('#button').on('click', function(e) {
-    // document.documentElement.requestFullscreen();
-    if (noteContent.length) {
-        noteContent = '';
-        noteTextarea.val('');
+recognition.continuous = true;
+recognition.start();
+
+// This block is called every time the Speech APi captures a line. Ф
+recognition.onresult = function(event) {
+    var current = event.resultIndex;
+    // Get a transcript of what was said.
+    var transcript = event.results[current][0].transcript;
+       sl = transcript.toLowerCase().replace(/[^0-9]/gim,'');
+       w = transcript.toLowerCase().replace(/[^т,с,р,t,s,r]/gim,'');
+        
+        console.log(w + sl)
+
+        switch (w) {
+            case 'т':
+            case 't':
+                getBarcode(t)
+                break;
+            case 'с':
+            case 's':
+                getBarcode(s)
+                break;
+            case 'р':
+            case 'r':
+                getBarcode(r)
+                    break;
+        
+            default:
+                break;
+        }
+   
+    var mobileRepeatBug = (current == 1 && transcript == event.results[0][0].transcript);
+    if (!mobileRepeatBug) {
+        noteTextarea.val(w + sl);
     }
+};
+
+$('#button').on('click', function(e) {
+    document.documentElement.requestFullscreen();
+    noteTextarea.val('Listening...');
+    document.getElementById('shtrihPicture').src = 'https://barcode.tec-it.com/barcode.ashx?data=' + slot + '&code=Code128&translate-esc=on';
     recognition.start();
 });
