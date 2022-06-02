97e1d9988cf58a56a565ccdad8fdffefcd5f065a
Geerun <bestgeerun@gmail.com>
2022-06-02 23:13:19 +0400

    add pereborka

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const r = [0, 0, 0, 0, 0, 0, 0, 0, 0, 'S200087909'];
 const p = [0, 0, 0, 0, 0, 0, 0, 0, 0, 'S200087773'];
 const o = [0, 0, 0, 0, 0, 0, 0, 0, 0, 'S200087638'];
 const a = [0, 0, 0, 0, 0, 0, 0, 0, 0, 'S200088313'];
-
+const generalArray = ['PER7G'];
 
 
 function makeSlot() {for (let i = 10; i <=142; i++){
@@ -19,7 +19,7 @@ function makeSlot() {for (let i = 10; i <=142; i++){
     r[i]=('S' + (200087900+i));
     p[i]=('S' + (200087764+i)); 
     o[i]=('S' + (200087629+i));
-    a[i]=('S' + (200088304+i));
+    a[i]=('S' + (200088305+i));
 }
 }
 makeSlot();
@@ -40,8 +40,10 @@ recognition.onresult = function(event) {
     // Get a transcript of what was said.
     var transcript = event.results[current][0].transcript;
        sl = transcript.toLowerCase().replace(/[^0-9]/gim,'');
-       w = transcript.toLowerCase().replace(/[^т,с,р,а,п,о,t,s,r,a,p,o]/gim,'');
+    //    w = transcript.toLowerCase().replace(/[^т,с,р,а,п,о,t,s,r,a,p,o]/gim,'');
+       w = transcript.toLowerCase().replace(/[^a-z,а-я]/gim,'');
 
+    if (w == "переборка"){sl = 0; getBarcode(generalArray)} else {
         switch (w[0]) {
             case 'т':
             case 't':
@@ -70,15 +72,16 @@ recognition.onresult = function(event) {
             default:
                 break;
         }
+    }
+
     var mobileRepeatBug = (current == 1 && transcript == event.results[0][0].transcript);
     if (!mobileRepeatBug) {
-        Textarea.textContent = (w[0].toUpperCase() + sl);
+        Textarea.textContent = (w == 'переборка' ? w : (w[0].toUpperCase() + sl));
     }
 };
 
 $('#button').on('click', function(e) {
-    document.documentElement.requestFullscreen();
+    // document.documentElement.requestFullscreen();
     Textarea.textContent = ('Listening...');
-    // document.getElementById('shtrihPicture').src = 'https://barcode.tec-it.com/barcode.ashx?data=' + 'generic' + '&code=Code128&translate-esc=on';
     recognition.start();
 });
